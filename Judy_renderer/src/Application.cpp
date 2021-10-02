ebb0723ca4982ab2788c7249a52d2273c77cbc8a
skorakora <skorski.6a.20.2012@wp.pl>
2021-10-02 02:32:46 +0100

    Added triangle renderer

--- Judy_renderer/src/Application.cpp
+++ Judy_renderer/src/Application.cpp
@@ -1,40 +1,103 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "Globals.h"
+#include "Shaders.h"
 
 int main(void)
 {
-    GLFWwindow* window;
-    global_variables global;
+	GLFWwindow* window;
+	global_variables global;
+	renderer_shaders shader;
 
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(global.screenWidith, global.screenHeight, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(global.screenWidith, global.screenHeight, "Judy Renderer", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
-    glfwTerminate();
-    return 0;
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		return -1;
+	}
+
+
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &shader.vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &shader.fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glUseProgram(shaderProgram);
+
+
+
+
+
+
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
