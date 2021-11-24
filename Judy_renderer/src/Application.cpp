b3095cd990edc387aca10a6b7dfa53419bf761b1
skorakora <skorski.6a.20.2012@wp.pl>
2021-11-24 20:37:55 +0000

    Uupdate: added second triangle

--- Judy_renderer/src/Application.cpp
+++ Judy_renderer/src/Application.cpp
@@ -1,4 +1,4 @@
-#include <glad/glad.h>
+﻿#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "Globals.h"
 #include "Shaders.h"
@@ -34,19 +34,31 @@ int main(void)
 	}
 
 
-	float vertices[] = {
+	float vertices1[] = {
 		 0.5f,  0.5f, 0.0f,  // top right
 		 0.5f, -0.5f, 0.0f,  // bottom right
 		-0.5f, -0.5f, 0.0f,  // bottom left
 		-0.5f,  0.5f, 0.0f   // top left 
 	};
-	unsigned int indices[] = {  // note that we start from 0!
+	unsigned int indices1[] = {  // note that we start from 0!
 		0, 1, 3,   // first triangle
 		1, 2, 3,   // second triangle
 		2, 3, 0
 	};
 
+	float vertices2[] = {
+	 0.2f,  0.4f, 0.0f,  // top right
+	 0.4f, -0.4f, 0.0f,  // bottom right
+	-0.4f, -0.4f, 0.0f,  // bottom left
+	-0.4f,  0.4f, 0.0f   // top left 
+	};
+	unsigned int indices2[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3,   // second triangle
+		2, 3, 0
+	};
 
+	
 
 
 	unsigned int vertexShader;
@@ -61,31 +73,64 @@ int main(void)
 	glCompileShader(fragmentShader);
 
 
+	unsigned int fragmentShader1;
+	fragmentShader1 = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader1, 1, &shader.fragmentShaderSource1, NULL);
+	glCompileShader(fragmentShader1);
+
+
 	unsigned int shaderProgram;
 	shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
 
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
+	unsigned int shaderProgram1;
+	shaderProgram1 = glCreateProgram();
+	glAttachShader(shaderProgram1, vertexShader);
+	glAttachShader(shaderProgram1, fragmentShader1);
+	glLinkProgram(shaderProgram1);
 
 
-	unsigned int EBO;
-	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	unsigned int VAO[2]; //vertex attribute
+	glGenVertexArrays(2, VAO);
+	
 
 
+	unsigned int EBO[2]; //EBO lol, zapomniałem co to było, ale to przechowuje informacje o kolejności trójkątów
+	glGenBuffers(2, EBO);
+
+	unsigned int VBO[2]; //Vertex buffer 
+	glGenBuffers(1, VBO);
+
+
+
+
+
+	//send data to GPU
+	glBindVertexArray(VAO[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices1), indices1, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
 	glEnableVertexAttribArray(0);
+
+
+
+	glBindVertexArray(VAO[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices2), indices2, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+
+	
 	glUseProgram(shaderProgram);
 
 
@@ -100,10 +145,21 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		//Draw Calls
 
+		//Judy->DrawCall OBJ1
 		glUseProgram(shaderProgram);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(VAO[0]);
+		glDrawElements(GL_TRIANGLES, sizeof(indices1), GL_UNSIGNED_INT, (void*)0);
+		glBindVertexArray(0);
+
+
+		//Judy->DrawCall OBJ2
+		glUseProgram(shaderProgram1);
+		glBindVertexArray(VAO[1]);
+		glDrawElements(GL_TRIANGLES, sizeof(indices1), GL_UNSIGNED_INT, (void*)0);
+		glBindVertexArray(0);
+
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
--- Judy_renderer/src/Shaders.h
+++ Judy_renderer/src/Shaders.h
@@ -17,5 +17,14 @@ struct renderer_shaders
 		"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 		"}\n";
 
+
+	const char* fragmentShaderSource1 = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+
+		"void main()\n"
+		"{\n"
+		"    FragColor = vec4(1.0f, 0.5f, 1f, 1.0f);\n"
+		"}\n";
+
 };
 
